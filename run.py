6e34b8e6527027cce9c0e7833ecc4002f28b620f
kpsdev1 <stuart151@hotmail.com>
2022-10-25 16:12:57 +0000

    creating typewriter fuunction

--- run.py
+++ run.py
@@ -2,10 +2,18 @@
 import random
 import sys
 import time
+
 deck = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 player_hand = []
 dealer_hand = []
 player_score = 0
 dealer_score = 0
 name = input('Please enter your name? ')
-print(f"Hi {name}, Welcome to the blackJack table.")
+
+def typewriter(words):
+    for c in words:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.15)
+       
+typewriter(f"Hi {name}, Welcome to the blackJack table.")
