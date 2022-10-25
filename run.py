9ce43df481432eb83df7764f3af26ef1fe1304d7
kpsdev1 <stuart151@hotmail.com>
2022-10-25 15:57:06 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import sys
+import time
+deck = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+player_hand = []
+dealer_hand = []
+player_score = 0
+dealer_score = 0
+name = input('Please enter your name? ')
+print(f"Hi {name}, Welcome to the blackJack table.")
