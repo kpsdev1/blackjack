3f425e06da522210bd3b9a6ad3fd2d0e66094afe
kpsdev1 <stuart151@hotmail.com>
2022-11-07 21:06:58 +0000

    fixing spelling mistake in rules

--- run.py
+++ run.py
@@ -63,8 +63,7 @@ def greeting():
     typewriter("4. Getting 21 is counted as blackJack no matter "
                "how many cards it takes.")
     print()
-    typewriter("5. If the player gets 21 with their first 2 cards they"
-               " automatically win")
+    typewriter("5. If the player gets 21 they automatically win")
     print()
     typewriter("6. Jack(J), Queen(Q) and King(K) all equal 10,"
                " Ace(A) is either 11 or 1 \ndepending on your score")
